62e8f46a82860404a2e849829c9a27d812fd651c
cherry <icecreamembracing@outlook.com>
2024-05-07 10:58:40 +0800

    提交5.7

--- vue-project/src/views/layout/content/content-index.vue
+++ vue-project/src/views/layout/content/content-index.vue
@@ -6,7 +6,7 @@
             顶部区域
         </h3>
         </div>
-      <!--可变内容，通过router-view重定向-->
+      <!--可变内容，通过router-view重定向111-->
       <div class="wrapper">
         <router-view></router-view>
       </div>
